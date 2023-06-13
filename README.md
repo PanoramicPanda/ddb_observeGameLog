e503aecbea0e80f62001af3ef0a5e53d440ab449
PanoramicPanda <uriah.h.brown@gmail.com>
2023-06-12 22:11:50 -0400

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # ddb_observeGameLog
-A Symbiote to watch the D&amp;D Beyond Game Log from a Campaign and report the rolls into TaleSpire.
+A Symbiote to watch the D&D Beyond Game Log from a Campaign and report the rolls into TaleSpire.
+
+# Loading into TaleSpire
+Place the `ddb_observeGameLog` folder into `%AppData%\..\LocalLow\BouncyRock Entertainment\TaleSpire\Symbiotes\`.
+Upon loading TaleSpire's Symbiotes, you will see one labeled **D&D Beyond Game Log Observer**.
+
+# Usage
+When loaded up, the Symbiote will attempt to load directly to https://www.dndbeyond.com/my-campaigns.
+You'll likely need to login, and perhaps prove you're human to D&D Beyond.
+You can then navigate to the campaign you wish to observe and open up the Game Log.
+
+![examples/game_log.png](example_images/game_log.png)
+
+Once the Game Log is opened, that's it! Any roll in made in D&D Beyond within that campaign will now report out to
+the TaleSpire Chat Log. The Symbiote even has permission to run in the background and will not need to stay open on
+your screen in order to report, so long as you don't close it from running overall.
+
+
+# Examples
+## Natural 1
+![example_images/ddb_ss.png](example_images/ddb_ss.png)
+![example_images/ts_ss.png](example_images/ts_ss.png)
+## Natural 20
+![example_images/ddb_int.png](example_images/ddb_int.png)
+![example_images/ts_int.png](example_images/ts_int.png)
+## Lots o' Dice
+![example_images/ddb_fireball.png](example_images/ddb_fireball.png)
+![example_images/ts_fireball.png](example_images/ts_fireball.png)
---
+++ observeGameLog.js
@@ -0,0 +1,104 @@
+var isObservingGameLog = false;
+var gameLogObserver = null;
+
+function observeGameLog() {
+    if (isObservingGameLog) return;
+
+    gameLogObserver = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+                const addedLi = mutation.addedNodes[0];
+                if (addedLi.tagName === 'LI') {
+                    const diceMessageElement = addedLi.querySelector('[class*="DiceMessage_Pending"]');
+                    if (!diceMessageElement) {
+                        const roll = extractDataFromLi(addedLi);
+
+                        let diceRoll = roll.diceRoll;
+                        let color = "<color=\"white\">";
+                        if (diceRoll.includes('d20')) {
+                            let diceResultExpression = roll.diceResultMath;
+                            let firstNumberString = diceResultExpression.split(' ')[0]; // Split the string by space and take the first element
+                            let firstNumber = parseInt(firstNumberString, 10); // Convert the string to a number
+
+                            if (firstNumber === 20) {
+                                color = "<color=\"green\">"; // Sets color to Green if a natural 20 is rolled
+                            } else if (firstNumber === 1) {
+                                color = "<color=\"red\">"; // Sets color to Red if a natural 1 is rolled
+                            }
+                        }
+
+                        TS.symbiote.sendNotification(roll.character, "<align=\"center\">" + roll.rollAction + " " + roll.rollType +
+                            "\n<size=200%>" + color + roll.diceResultTotal + "</color>\n<size=90%>" + roll.diceRoll + "  (" + roll.diceResultMath + ")");
+                        console.log(roll);
+                    }
+                }
+            }
+        });
+    });
+
+    const gameLogElement = document.querySelector('[class*="GameLog_GameLogEntries"]');
+    if (gameLogElement) {
+        gameLogObserver.observe(gameLogElement, {childList: true});
+        isObservingGameLog = true;
+    } else {
+        console.error('GameLog_GameLogEntries not found');
+    }
+}
+
+function stopObservingGameLog() {
+    if (gameLogObserver) {
+        gameLogObserver.disconnect();
+        isObservingGameLog = false;
+    }
+}
+
+function checkAndObserveGameLog() {
+    const gameLogElement = document.querySelector('[class*="GameLog_GameLogEntries"]');
+    if (gameLogElement) {
+        observeGameLog();
+    } else {
+        stopObservingGameLog();
+    }
+}
+
+function capitalizeWords(str) {
+    return str.replace(/\b\w+/g, word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+}
+
+function extractDataFromLi(liElement) {
+    // Dice Roll
+    const diceRollElement = liElement.querySelector('[class*="DiceMessage_Notation"] span');
+    const diceRoll = diceRollElement ? diceRollElement.textContent : '';
+
+    // Dice Result Math
+    const diceResultMathElement = liElement.querySelector('[class*="DiceMessage_Number"]');
+    const diceResultMath = diceResultMathElement ? diceResultMathElement.textContent : '';
+
+    // Dice Result Total
+    const diceResultTotalElement = liElement.querySelector('[class*="DiceMessage_Total"] span');
+    const diceResultTotal = diceResultTotalElement ? diceResultTotalElement.textContent : '';
+
+    // Character
+    const characterElement = liElement.querySelector('[class*="GameLogEntry_Sender"]');
+    const character = characterElement ? characterElement.textContent : '';
+
+    // Roll Action
+    const rollActionElement = liElement.querySelector('[class*="DiceMessage_Action"]');
+    const rollAction = rollActionElement ? rollActionElement.textContent : '';
+
+    // Roll Type
+    const rollTypeElement = liElement.querySelector('[class*="DiceMessage_RollType"]');
+    const rollType = rollTypeElement ? capitalizeWords(rollTypeElement.textContent) : '';
+
+    return {
+        diceRoll,
+        diceResultMath,
+        diceResultTotal,
+        character,
+        rollAction,
+        rollType
+    };
+}
+
+// Call checkAndObserveGameLog every 5 seconds
+setInterval(checkAndObserveGameLog, 5000);
